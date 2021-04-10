1d059f59ace3e83e04f540ea383d2ba9732114bc
Alfonso Irazabal <alfoirazabal@live.com>
2021-04-10 16:43:40 -0300

    Test runs can now be deleted

--- app/src/main/java/com/alfoirazabal/studyquizmaker/gui/test/panel/testrun/results/recyclerviews/AdapterTestRun.java
+++ app/src/main/java/com/alfoirazabal/studyquizmaker/gui/test/panel/testrun/results/recyclerviews/AdapterTestRun.java
@@ -1,16 +1,22 @@
 package com.alfoirazabal.studyquizmaker.gui.test.panel.testrun.results.recyclerviews;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
 
+import com.alfoirazabal.studyquizmaker.AppConstants;
 import com.alfoirazabal.studyquizmaker.R;
+import com.alfoirazabal.studyquizmaker.db.AppDatabase;
 import com.alfoirazabal.studyquizmaker.domain.testrun.TestRun;
 import com.alfoirazabal.studyquizmaker.gui.test.panel.testrun.results.ViewRunResults;
 
@@ -30,16 +36,44 @@ public class AdapterTestRun extends RecyclerView.Adapter<AdapterTestRun.ViewHold
 
             this.txtScore = itemView.findViewById(R.id.txt_score);
             this.progressBarScore = itemView.findViewById(R.id.progressbar_score);
+            ImageView imgBtnDelete = itemView.findViewById(R.id.imgbtn_delete);
+
             this.progressBarScore.setMax(100);
 
             itemView.setOnClickListener(v -> {
                 int itemPosition = getAdapterPosition();
-                TestRun testRun = testRuns.get(itemPosition);
+                TestRun testRunInPosition = testRuns.get(itemPosition);
                 Intent intentViewRunResults = new Intent(
                     itemView.getContext(), ViewRunResults.class
                 );
-                intentViewRunResults.putExtra("TESTRUNID", testRun.id);
+                intentViewRunResults.putExtra("TESTRUNID", testRunInPosition.id);
                 itemView.getContext().startActivity(intentViewRunResults);
+            });
+
+            imgBtnDelete.setOnClickListener(v -> {
+                int itemPosition = getAdapterPosition();
+                TestRun testRunInPosition = testRuns.get(itemPosition);
+                Context context = itemView.getContext();
+                String testrunDeletionDescription =
+                        context.getString(R.string.msg_delete_confirmation_testrun);
+                new AlertDialog.Builder(itemView.getContext())
+                        .setTitle(context.getString(R.string.delete_confirmation))
+                        .setMessage(testrunDeletionDescription)
+                        .setIcon(android.R.drawable.ic_delete)
+                        .setPositiveButton(context.getString(R.string.yes), (dialog, which) -> {
+                            new Thread(() -> {
+                                AppDatabase db = Room.databaseBuilder(
+                                        itemView.getContext(),
+                                        AppDatabase.class,
+                                        AppConstants.getDBLocation(context)
+                                ).build();
+                                db.testRunDAO().delete(testRunInPosition);
+                            }).start();
+                            testRuns.remove(testRunInPosition);
+                            notifyDataSetChanged();
+                        })
+                        .setNegativeButton(context.getString(R.string.no), null)
+                        .show();
             });
         }
 
